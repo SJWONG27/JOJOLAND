58813805fabfdb9f35f8a61b496e37b5cbaaf2e8
arinahazam <116658725+arinahazam@users.noreply.github.com>
2023-06-14 22:44:05 +0800

    Update TheGoldenSpirit.java

--- TheGoldenSpirit/src/main/java/com/mycompany/thegoldenspirit/TheGoldenSpirit.java
+++ TheGoldenSpirit/src/main/java/com/mycompany/thegoldenspirit/TheGoldenSpirit.java
@@ -8,79 +8,158 @@ package com.mycompany.thegoldenspirit;
  *
  * @author Hewlett Packard
  */
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class Node {
-    String input;
-    Node left, right;
+class GraphNode {
+    String name;
+    List<GraphNode> neighbours;
 
-    public Node(String item) {
-        input = item;
-        left = right = null;
+    public GraphNode(String name) {
+        this.name = name;
+        this.neighbours = new ArrayList<>(); //graph is connected
     }
 }
 
-public class TheGoldenSpirit {
-    Node root;
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+        GraphNode georgeImary = new GraphNode("George Joestar I and Mary Joestar");
+        GraphNode jonathan = new GraphNode("Jonathan Joestar");
+        GraphNode erina = new GraphNode("Erina Joestar");
+        GraphNode dio = new GraphNode("DIO");
+        GraphNode georgeII = new GraphNode("George Joestar II");
+        GraphNode lisa = new GraphNode("Lisa Lisa");
+        GraphNode giorno = new GraphNode("Giorno Giovanna");
+        GraphNode joseph = new GraphNode("Joseph Joestar");
+        GraphNode suzi = new GraphNode("Suzi Q");
+        GraphNode tomoko = new GraphNode("Tomoko Higashikata");
+        GraphNode josuke = new GraphNode("Josuke Higashikata");
+        GraphNode holy = new GraphNode("Holy Kujo");
+        GraphNode sadao = new GraphNode("Sadao Kujo");
+        GraphNode jotaro = new GraphNode("Jotaro Kujo");
+        GraphNode jolyne = new GraphNode("Jolyne Cujoh");
 
-    Node findLCA(String n1, String n2) {
-        return findLCAUtil(root, n1, n2);
+        //The relationships of each joestar where neighbours=child
+        georgeImary.neighbours.add(jonathan);
+        jonathan.neighbours.add(georgeII);
+        jonathan.neighbours.add(giorno);
+        erina.neighbours.add(georgeII);
+        dio.neighbours.add(giorno);
+        georgeII.neighbours.add(joseph);
+        lisa.neighbours.add(joseph);
+        joseph.neighbours.add(holy);
+        joseph.neighbours.add(josuke);
+        tomoko.neighbours.add(josuke);
+        suzi.neighbours.add(holy);
+        sadao.neighbours.add(jotaro);
+        holy.neighbours.add(jotaro);
+        jotaro.neighbours.add(jolyne);
+
+        // Ask user to enter two names
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the names of first Joestar: ");
+        String joestarName1 = scanner.nextLine();
+        System.out.print("Enter the names of second Joestar: ");
+        String joestarName2 = scanner.nextLine();
+
+        //Root is georgeImary because they are the first
+        GraphNode node1 = findNode(georgeImary, joestarName1);
+        GraphNode node2 = findNode(georgeImary, joestarName2);
+        
+        //conditions to find and print lowest common ancestor(lca)
+        if (node1 != null && node2 != null) { //makes sure names are valid
+            GraphNode lca = findLCA(georgeImary, node1, node2);
+            if (lca != null) {
+                System.out.println("===================================================");
+                System.out.print("Lowest Common Ancestor: " + lca.name);
+                //for those with spouses, extra condition apply:
+                if (lca.name.equals("Holy Kujo")) {
+                    System.out.print(" and Sadao Kujo");
+                } else if (lca.name.equals("George Joestar II")) {
+                    System.out.print(" and Lisa Lisa");
+                }
+                System.out.println();
+                System.out.println("===================================================");
+            } else {
+                System.out.println("No common ancestor found.");
+            }
+        } else {
+            System.out.println("Invalid Joestar names.");
+        }
     }
 
-    Node findLCAUtil(Node node, String n1, String n2) {
-        if (node == null) {
+    public static GraphNode findNode(GraphNode root, String nodeName) {
+        if (root == null || nodeName == null) {
             return null;
         }
 
-        if (node.left != null && (node.left.input.equals(n1) || node.left.input.equals(n2))) {
-            return node;
+        if (root.name.equals(nodeName)) {
+            return root; 
+        }
+        //recursively find for nodeName under neighbours
+        for (GraphNode neighbour : root.neighbours) {
+            GraphNode node = findNode(neighbour, nodeName);
+            if (node != null) {
+                return node;
+            }
         }
 
-        if (node.right != null && (node.right.input.equals(n1) || node.right.input.equals(n2))) {
-            return node;
+        return null;
+    }
+    //method to find LCA
+    public static GraphNode findLCA(GraphNode root, GraphNode node1, GraphNode node2) {
+        if (root == null || node1 == null || node2 == null) {
+            return null;
         }
 
-        Node leftLCA = findLCAUtil(node.left, n1, n2);
-        Node rightLCA = findLCAUtil(node.right, n1, n2);
-
-        if (leftLCA != null && rightLCA != null) {
-            return node;
+        if (root == node1 || root == node2) { //either one is ancestor of itself so return null
+            return null;
         }
 
-        if (leftLCA != null) {
-            return leftLCA;
+        GraphNode lca = null;
+
+        for (GraphNode neighbor : root.neighbours) {
+            GraphNode temp = findLCA(neighbor, node1, node2);
+
+            if (temp != null) {
+                if (lca != null) {
+                    // if found another common ancestor, return root
+                    return root;
+                }
+                lca = temp;
+            }
         }
 
-        if (rightLCA != null) {
-            return rightLCA;
+        if (lca != null && lca != node1 && lca != node2) {
+            return lca;
+        }
+
+        boolean node1Found = isNodePresent(root, node1);
+        boolean node2Found = isNodePresent(root, node2);
+
+        if (node1Found && node2Found) {
+            return root;
         }
 
         return null;
     }
 
-    public static void main(String args[]) {
-        TheGoldenSpirit tree = new TheGoldenSpirit();
-        tree.root = new Node("George Joestar I and Mary Joestar");
-        tree.root.left = new Node("Jonathan Joestar");
-        tree.root.left.left = new Node("George Joestar II");
-        tree.root.left.right = new Node("Giorno Giovanna");
-        tree.root.left.left.left = new Node("Joseph Joestar");
-        tree.root.left.left.left.left = new Node("Holy Kujo");
-        tree.root.left.left.left.right = new Node("Josuke Higashikata");
-        tree.root.left.left.left.left.left = new Node("Jotaro Kujo");
-        tree.root.left.left.left.left.left.left = new Node("Jolyne Cujoh");
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the name of the first Joestar: ");
-        String name1 = scanner.nextLine();
-        System.out.print("Enter the name of the second Joestar: ");
-        String name2 = scanner.nextLine();
-
-        Node lca = tree.findLCA(name1, name2);
-        if (lca != null) {
-            System.out.printf("Lowest Common Ancestor of %s and %s is: %s\n", name1, name2, lca.input);
-        } else {
-            System.out.println("Name entered is not in the Joestar family.");
+    public static boolean isNodePresent(GraphNode root, GraphNode node) {
+        if (root == null || node == null) { //graph doesnt exist
+            return false;
         }
+
+        if (root == node) { //root found, means present
+            return true;
+        }
+
+        for (GraphNode neighbor : root.neighbours) { //serach for node recursively in reighboyurs
+            if (isNodePresent(neighbor, node)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
